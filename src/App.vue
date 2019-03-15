95b7884819743d972ab7714315daad50e2faccab
Akash <subs@appsparkler.com>
2019-03-15 16:30:09 +0530

    -copied the LogoAuthor component files to project - successfully `npm run serve` - issue is that hot-reload is not working -

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,16 @@ include AppTemplate.pug
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LogoAuthor from './components/global/LogoAuthor';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    LogoAuthor
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Global/LogoAuthor/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+include LogoAuthor.pug
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+a.navbar-brand {
+  height:108px;
+  width: 143px;
+  display:block;
+  border: 1px red solid;
+}
+</style>
