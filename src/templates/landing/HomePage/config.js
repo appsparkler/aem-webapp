6a8e6214457336d63ceb3f45d2d09f57da3682d5
Akash <subs@appsparkler.com>
2019-03-18 16:31:41 +0530

    updating files, deleting non-minimalistic files

--- src/templates/landing/HomePage/config.js
+++
@@ -1,9 +0,0 @@
-import HomePage_Card from './template-components/HomePage_Card';
-import HomePage_Card1 from './template-components/HomePage_Card1';
-import HomePage_Card2 from './template-components/HomePage_Card2';
-
-export default {
-    HomePage_Card,
-    HomePage_Card1,
-    HomePage_Card2
-}
