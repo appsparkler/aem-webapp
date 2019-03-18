90bb9dc96d38ac6da1e28045566d06269512afb2
Akash <subs@appsparkler.com>
2019-03-18 07:41:27 +0530

    - prepared template for home page - installed bootstrap, jquery and popper

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap\\dist\\css\\bootstrap.css'
+import 'jquery'
+import 'bootstrap'
 
 Vue.config.productionTip = false
+Vue.config.ignoreElements = ['sly'];
 
 new Vue({
     render: h => h(App)
