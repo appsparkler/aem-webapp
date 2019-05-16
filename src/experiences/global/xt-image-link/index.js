01586001db62720c25f1a124afa784f479317319
Akash <subs@appsparkler.com>
2019-05-16 06:49:07 +0530

    added an xt-image-link component

---
+++ src/experiences/global/xt-image-link/index.js
@@ -0,0 +1,56 @@
+import Vue from 'vue';
+import $ from 'jquery';
+import LogoComponent from 'components/global/LogoAuthor';
+
+// Vue.component('global-logo-author', {
+//   template: `
+//     <h2>The Navbar...</h2>
+//   `
+// })
+
+new Vue({
+  el: '#xt_navbar',
+  template: $('#xt_navbar').get(0).outerHTML
+})
+
+// const $navbars = $('[id^=xt_navbar]');
+//
+// $navbars.each(function(idx, el) {
+//     setup_logoComponent.apply(null, arguments);
+//     var navbarTemplate = get_navbarTemplate.apply(this, arguments);
+//     new Vue({
+//       el,
+//       template: navbarTemplate
+//     })
+// });
+
+function setup_logoComponent(idx, el) {
+    try {
+        const componentTemplate = $('.vue-logo-component', el).get(0).outerHTML;
+        new LogoComponent({
+          template: componentTemplate
+        });
+    } catch (e) {
+      console.error(e);
+    }
+}
+
+function get_navbarTemplate(idx, elem) {
+  try {
+    const navbarTempalte = elem.outerHTML;
+    return elem.outerHTML;
+  } catch (e) {
+      console.log('error in getting navbar-template');
+  }
+}
+
+function get_vueLogoComponent(idx, elem) {
+    try {
+      const logoComponentTemplate = $('.vue-logo-component', elem).get(0).outerHTML;
+      LogoComponent.template = logoComponentTemplate;
+      const vueLogoComponent = Vue.component('logo', LogoComponent);
+      return vueLogoComponent;
+    } catch (e) {
+        console.log('error in get_vueLogoComponent');
+    }
+}
