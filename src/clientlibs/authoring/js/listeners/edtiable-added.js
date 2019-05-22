8748271ea77f85fbc0bd42568a7cc0154e3940ca
Akash <subs@appsparkler.com>
2019-05-22 07:11:53 +0530

    added clientlibs for adding the nodes when any component(inspectable) is added

---
+++ src/clientlibs/authoring/js/listeners/edtiable-added.js
@@ -0,0 +1,19 @@
+(function($document, $) {
+  $document.on('cq-inspectable-added', cb_inspectableAdded);
+  function cb_inspectableAdded(evt) {
+  	try {
+  		var inspectable = evt.inspectable;
+  		var ajaxConfig = {
+  			url: evt.inspectable.path,
+  			data: {
+  				'sling:resourceType': inspectable.type
+  			},
+  			async: false	
+  		};
+
+  		if(inspectable.type.indexOf('myApp/components') > -1) $.post(ajaxConfig);
+  	} catch(e) {
+  		console.error(e);
+  	}	
+  }
+}($(document), jQuery));
