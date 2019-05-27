dba8db2bc918f83bcc911d47a210a55dfe4fb2f1
Akash <subs@appsparkler.com>
2019-05-27 16:43:40 +0530

    Build WIP

--- src/templates/global/BasePage/main.js
+++ src/templates/global/BasePage/main.js
@@ -5,10 +5,6 @@ import 'bootstrap'
 //
 import Vue from 'vue';
 //
-import {initialize_VueApps} from 'common-script'
-import {setup_XTImageLinkComponents} from 'experiences/global/xt-navbar/index.js';
-import {setup_XTNavbarComponents} from 'experiences/global/xt-image-link/index.js'
+import { initialize_VueApps } from 'common-script'
 
-// setup_XTImageLinkComponents()
-// setup_XTNavbarComponents()
 initialize_VueApps()
