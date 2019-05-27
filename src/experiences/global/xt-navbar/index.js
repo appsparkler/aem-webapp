749101870e4ce4b15a2d38db2485662bb3c405b4
Akash <subs@appsparkler.com>
2019-05-27 16:22:04 +0530

    build WIP

--- src/experiences/global/xt-navbar/index.js
+++ src/experiences/global/xt-navbar/index.js
@@ -5,8 +5,8 @@ import {initialize_VueApps, generate_xtComponent} from 'common-script';
 import {generate_imageLinkComponent} from 'aem-components/global/image-link';
 
 setup_XTNavbarComponents();
-
 if(process.env.VUE_APP_IS_DEV) initialize_VueApps();
+
 export function setup_XTNavbarComponents() {
     $('[is^=image-link]', '[is^=xt-navbar]').each(generate_imageLinkComponent);
     $('[is^=xt-navbar]').each(generate_xtNavbarComponent);
