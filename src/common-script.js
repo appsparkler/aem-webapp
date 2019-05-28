0a3b6108d2fc17ad7a7e1d6f79c05a06ed449198
Akash <subs@appsparkler.com>
2019-05-28 11:19:38 +0530

    build WIP

--- src/common-script.js
+++ src/common-script.js
@@ -8,16 +8,6 @@ export function initialize_VueApps() {
   $('[id^=app]').each(VueApp);
 }
 
-export function generate_xtComponent(el, options) {
-    const $this = el;
-    var vue_componentName = $this.attributes.is.value;
-    var vue_template = $this.outerHTML.replace(/is=".*?"/, "").toString(); // avoid "maximum-call-stack-size-exceeded"
-    options = options || {};
-    options.name = vue_componentName;
-    options.template = vue_template;
-    Vue.component(vue_componentName, options);
-}
-
 function VueApp() {
     Vue.config.comments = true;
     Vue.config.ignoredElements = ['cq', 'sly'];
@@ -25,12 +15,10 @@ function VueApp() {
         console.log(msg);
     };
     Vue.config.productionTip = false;
-    console.log(this.outerHTML);
     var VueApp = new Vue({
         el: this,
         template: this.outerHTML
     });
-    console.log(VueApp)
 }
 
 export class VueAEMComponent {
