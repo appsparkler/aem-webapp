53f88ed597de37d0ed2d5e0df409e3bc4dc53681
Akash <subs@appsparkler.com>
2019-05-30 13:11:10 +0530

    Build WIP

--- src/templates/global/BasePage/index.js
+++ src/templates/global/BasePage/index.js
@@ -1,10 +1,3 @@
-import 'bootstrap\\dist\\css\\bootstrap.css'
-import '@fortawesome/fontawesome-free/css/all.css'
-import $ from 'jquery'
-import 'bootstrap'
-//
-import Vue from 'vue';
-//
 import { initialize_VueApps } from 'common-script'
-
-initialize_VueApps()
+//
+initialize_VueApps();
