a18dac332a37798d399bf84b42dca543a288721d
Akash <subs@appsparkler.com>
2019-06-04 11:17:50 +0530

    Build WIP

--- src/experiences/global/xt-navbar/index.js
+++ src/experiences/global/xt-navbar/index.js
@@ -1,11 +1,11 @@
 import $ from 'jquery';
 import Vue from 'vue';
 //
-import XTNavbarComponentConfig from './vueComponentConfig'
-// import {initialize_VueApps, VueAEMComponent} from 'common-script';
+import XTNavbarComponentConfig from './xt-navbar-config'
 global.vueComponents = global.vueComponents || [];
 setup_XTNavbarComponents();
 
+// private functions
 function setup_XTNavbarComponents() {
     $('[is^=xt-navbar]').each(generate_xtNavbarComponent);
 }
