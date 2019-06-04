2102c61c58f297a2c527adc24fe0ee1de5750147
Akash <subs@appsparkler.com>
2019-06-04 11:25:20 +0530

    Build WIP

--- src/experiences/global/xt-navbar/VueComponentGenerator.js
+++
@@ -1,7 +0,0 @@
-export default class VueComponentNameAndTemplateExtractor {
-  constructor(el) {
-    const outerHTML = new String(el.outerHTML);
-    this.name = el.attributes.is.value;
-    this.template = outerHTML.replace(/is=".*?"/, "").toString()
-  }
-}
